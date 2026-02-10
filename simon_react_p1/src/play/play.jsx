b2efed40b376e0f0182d6fa75d38a3b8dca9c52e
wahwah2011 <163032484+wahwah2011@users.noreply.github.com>
2026-02-10 11:59:17 -0700

    Enhance About, Login, Play, and Scores components with detailed content and improved layout. Added images, forms, and a score table for better user engagement and experience.

--- simon_react_p1/src/play/play.jsx
+++ simon_react_p1/src/play/play.jsx
@@ -1,9 +1,40 @@
 import React from "react";
+import "./play.css";
 
 export function Play() {
   return (
-    <main className="container-fluid bg-secondary text-center">
-      <div>play displayed here</div>
+    <main className="bg-secondary">
+      <div className="players">
+        Player
+        <span className="player-name">Mystery player</span>
+        <div id="player-messages">
+          <div className="event">
+            <span className="player-event">Linus</span> scored 377
+          </div>
+          <div className="event">
+            <span className="player-event">Linus</span> started a new game
+          </div>
+          <div className="event">
+            <span className="system-event">game</span> connected
+          </div>
+        </div>
+      </div>
+
+      <div className="game">
+        <div className="button-container">
+          <button className="button-top-left"></button>
+          <button className="button-top-right"></button>
+          <button className="button-bottom-left"></button>
+          <button className="button-bottom-right"></button>
+          <div className="controls center">
+            <div className="game-name">
+              Simon<sup>&reg;</sup>
+            </div>
+            <div className="score center">--</div>
+            <button className="btn btn-primary">Reset</button>
+          </div>
+        </div>
+      </div>
     </main>
   );
 }
