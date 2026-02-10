af89bf6732c023a39a98bd85d3424e5d60b18a97
wahwah2011 <163032484+wahwah2011@users.noreply.github.com>
2026-02-10 11:49:09 -0700

    Refactor app component to use React Router for navigation, replacing static links with NavLink components. Added routes for Login, Play, Scores, About, and a NotFound page. Improved overall structure for better routing and user experience.

--- simon_react_p1/src/app.jsx
+++ simon_react_p1/src/app.jsx
@@ -2,54 +2,74 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./app.css";
 
+import { BrowserRouter, Routes, Route, NavLink } from "react-router-dom";
+import { Login } from "./login/login";
+import { Play } from "./play/play";
+import { Scores } from "./scores/scores";
+import { About } from "./about/about";
+
 export default function App() {
   return (
-    <div className="body bg-dark text-light">
-      <header className="container-fluid">
-        <nav className="navbar fixed-top navbar-dark">
-          <a className="navbar-brand" href="#">
-            Simon<sup>&reg;</sup>
-          </a>
-          <menu className="navbar-nav">
-            <li className="nav-item">
-              <a className="nav-link active" href="index.html">
-                Home
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="play.html">
-                Play
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="scores.html">
-                Scores
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="about.html">
-                About
-              </a>
-            </li>
-          </menu>
-        </nav>
-      </header>
+    <BrowserRouter>
+      <div className="body bg-dark text-light">
+        <header className="container-fluid">
+          <nav className="navbar fixed-top navbar-dark">
+            <div className="navbar-brand">
+              Simon<sup>&reg;</sup>
+            </div>
+            <menu className="navbar-nav">
+              <li className="nav-item">
+                <NavLink className="nav-link active" to="/">
+                  Home
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink className="nav-link" to="play">
+                  Play
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink className="nav-link" to="scores">
+                  Scores
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink className="nav-link" to="about">
+                  About
+                </NavLink>
+              </li>
+            </menu>
+          </nav>
+        </header>
 
-      <main className="container-fluid bg-secondary text-center">
-        App Components Go Here{" "}
-      </main>
+        <Routes>
+          <Route path="/" element={<Login />} exact />
+          <Route path="/play" element={<Play />} />
+          <Route path="/scores" element={<Scores />} />
+          <Route path="/about" element={<About />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
 
-      <footer className="bg-dark text-white-50">
-        <div className="container-fluid">
-          <span className="text-reset">Author Name(s)</span>
-          <a
-            className="text-reset"
-            href="https://github.com/webprogramming260/simon-css"
-          >
-            Source
-          </a>
-        </div>
-      </footer>
-    </div>
+        <footer className="bg-dark text-white-50">
+          <div className="container-fluid">
+            <span className="text-reset">Author Name(s)</span>
+            <a
+              className="text-reset"
+              href="https://github.com/webprogramming260/simon-css"
+            >
+              Source
+            </a>
+          </div>
+        </footer>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+function NotFound() {
+  return (
+    <main className="container-fluid bg-secondary text-center">
+      <div>404 - Page Not Found</div>
+    </main>
   );
 }
