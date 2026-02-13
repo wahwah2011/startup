ee912438e564254ab7e4a004b86a616efa2062f7
wahwah2011 <163032484+wahwah2011@users.noreply.github.com>
2026-02-12 18:25:22 -0700

    deleted simon react

--- simon_react_p1/index.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./src/app";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "./src/app.css";
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
