03b185459a04871b1d45f4d6dbaa2a670a9ac94a
wahwah2011 <163032484+wahwah2011@users.noreply.github.com>
2026-03-11 14:55:04 -0600

    Add trivia functionality to About component and update styles

--- reaction_time/service/index.js
+++ reaction_time/service/index.js
@@ -10,7 +10,7 @@ const authCookieName = "token";
 let users = [];
 let scores = [1, 2, 3];
 
-const port = process.argv.length > 2 ? process.argv[2] : 3000;
+const port = process.argv.length > 2 ? process.argv[2] : 4000;
 
 /*Add this code to service/index.js to cause Express static middleware to 
 serve files from the public directory once your code has been deployed to 
