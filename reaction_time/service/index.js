f70a8862cd875694f6cfee96e06b3d1bd183438f
wahwah2011 <163032484+wahwah2011@users.noreply.github.com>
2026-04-02 12:27:41 -0600

    Wire peerProxy into Express server to listen for WebSocket connections

--- reaction_time/service/index.js
+++ reaction_time/service/index.js
@@ -126,9 +126,13 @@ app.use((_req, res) => {
   res.sendFile("index.html", { root: "public" });
 });
 
-app.listen(port, () => {
+const { peerProxy } = require('./peerProxy.js');
+
+const httpService = app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
+
+peerProxy(httpService);
 
 // --- Helper functions ---
 
